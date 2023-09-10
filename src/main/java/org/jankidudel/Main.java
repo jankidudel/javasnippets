edb16fdcda1d2a445d324211bf212270c7de4ff2
jankidudel <jankidudel@gmail.com>
2023-09-11 00:32:49 +0300

    merge sort

--- src/main/java/org/jankidudel/Main.java
+++ src/main/java/org/jankidudel/Main.java
@@ -2,11 +2,22 @@ package org.jankidudel;
 
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) {
-        Integer[] toSort = {10, 41, 7, 3, 99};
-        Integer[] response = (new BubbleSort()).sort(toSort);
-        System.out.println(Arrays.toString(response));
+//        Integer[] bubbleSortInput = {10, 41, 7, 3, 99};
+//        Integer[] bubbleSortResponse = (new BubbleSort()).sort(bubbleSortInput);
+//        System.out.println("Bubble sort response: " + Arrays.toString(bubbleSortResponse));
+
+
+
+        // [3, 1, 50, 14, 33, 99, 2, 117, 44, 15])
+
+        Integer[] mergeSortInput = {3, 1, 50, 14, 33, 99, 2, 117, 44, 15};
+        Integer[] mergeSortResponse = (new MergeSort()).sort(mergeSortInput);
+        System.out.println("Merge sort response: " + Arrays.toString(mergeSortResponse));
+
+
+
+
     }
 }
---
+++ src/main/java/org/jankidudel/MergeSort.java
@@ -0,0 +1,41 @@
+package org.jankidudel;
+
+class MergeSort {
+
+    public Integer[] sort(Integer[] input) {
+
+        int inputSize = input.length;
+
+        Integer[] inputCopy = input.clone();
+        this.topDownSplitMerge(input, 0, inputSize, inputCopy);
+
+        return input;
+
+    }
+
+    private void topDownSplitMerge(Integer[] inputCopy, int iBegin, int iEnd, Integer[] input) {
+        if (iEnd - iBegin <= 1) {
+            return;
+        }
+
+        int iMiddle = (iEnd + iBegin) / 2;
+        topDownSplitMerge(input, iBegin, iMiddle, inputCopy);
+        topDownSplitMerge(input, iMiddle, iEnd, inputCopy);
+        topDownMerge(inputCopy, iBegin, iMiddle, iEnd, input);
+    }
+
+    private void topDownMerge(Integer[] inputCopy, int iBegin, int iMiddle, int iEnd, Integer[] input) {
+        int i = iBegin;
+        int j = iMiddle;
+
+        for (int k = iBegin; k < iEnd; k++) {
+            if (i < iMiddle && (j >= iEnd || input[i] <= input[j])) {
+                inputCopy[k] = input[i];
+                i++;
+            } else {
+                inputCopy[k] = input[j];
+                j++;
+            }
+        }
+    }
+}
