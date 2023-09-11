ae924c78b737ca3f1a729801030425e4e89d652c
jankidudel <jankidudel@gmail.com>
2023-09-11 22:35:32 +0300

    eu2

---
+++ src/main/java/org/jankidudel/eu/Problem2.java
@@ -0,0 +1,42 @@
+package org.jankidudel.eu;
+
+public class Problem2 {
+
+    private int maxTerm = 4000000; // 4 million
+
+    public Problem2() {
+    }
+
+    public Problem2(int maxTerm) {
+        this.maxTerm = maxTerm;
+    }
+
+    public static void main(String[] args) {
+        Problem2 problem1 = new Problem2();
+        int result = problem1.run();
+        System.out.println("Result = " + result);
+    }
+
+    public int run() {
+
+        int prevTerm = 0;
+        int lastTerm = 1;
+        int sum = 0;
+        while (lastTerm <= this.maxTerm) {
+            int tmpSwap = prevTerm;
+            prevTerm = lastTerm;
+            lastTerm += tmpSwap;
+
+            if (lastTerm > this.maxTerm) {
+                break;
+            }
+            if (lastTerm % 2 == 0) {
+                sum += lastTerm;
+            }
+//            System.out.println("tmpSwap = " + tmpSwap);
+//            System.out.println("last term = " + lastTerm);
+//            System.out.println("prev term = " + prevTerm);
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/org/jankidudel/eu/Problem2Test.java
@@ -0,0 +1,12 @@
+package org.jankidudel.eu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Problem2Test {
+    @Test
+    public void run() {
+        Assertions.assertEquals(44, (new Problem2(100).run()));
+        Assertions.assertEquals(4613732, (new Problem2(4000000).run()));
+    }
+}
